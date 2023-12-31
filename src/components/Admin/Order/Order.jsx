d2f2e3ddda29580db7f9572cb86320811f00d6ed
SIDDHARTH SUTARIYA <siddharthpsutariya@gmail.com>
2024-01-01 03:31:07 +0530

    Archita_S1

--- src/components/Admin/Order/Order.jsx
+++ src/components/Admin/Order/Order.jsx
@@ -1,3 +1,91 @@
+// import {
+//   Table,
+//   Thead,
+//   Tbody,
+//   Tr,
+//   Th,
+//   Td,
+//   TableContainer,
+// } from "@chakra-ui/react";
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+
+
+// const Order = () => {
+//   const [total, setTotal] = useState(0);
+
+//   const [data, setData] = React.useState([]);
+
+
+//   React.useEffect(() => {
+//     getCartData();
+//   }, []);
+
+//   const getCartData = async (params) => {
+  
+//     let res = await axios("/cart/getcart");
+    
+//     const d = await res.data;
+   
+//   };
+
+//   return (
+//     <div>
+//       <h1
+//         style={{
+//           textAlign: "center",
+//           marginTop: "10px",
+//           fontSize: "20px",
+//           fontWeight: "bold",
+//         }}
+//       >
+//         Recent Orders
+//       </h1>
+//       <TableContainer>
+//         <Table variant="simple">
+//           <Thead>
+//             <Tr>
+//               <Th>Id</Th>
+//               <Th>Order ID</Th>
+//               <Th>Title</Th>
+//               <Th isNumeric>Total Products</Th>
+//             </Tr>
+//           </Thead>
+//           <Tbody>
+//             {data?.map((items) => {
+//               return (
+//                 <Tr>
+//                   <Td fontFamily="'Philosopher', sans-serif;">{items.id}</Td>
+//                   <Td>{items.orderId}</Td>
+
+//                   {items.products.map((e) => (
+//                     <Tr>
+//                       <Th>Products</Th>
+//                       <Td>{e.title}</Td>
+//                       <Th>Price</Th>
+//                       <Td>₹{e.price}</Td>
+//                       <Th>Status</Th>
+//                       <Td style={{ borderRadius: "20px", background: "green" }}>
+//                         {e.status}
+//                       </Td>
+//                     </Tr>
+//                   ))}
+//                   <Td>{items.products.length}</Td>
+//                 </Tr>
+//               );
+//             })}
+//           </Tbody>
+//         </Table>
+//       </TableContainer>
+//     </div>
+//   );
+// };
+
+// export default Order;
+
+
 import {
   Table,
   Thead,
@@ -57,10 +145,9 @@ const Order = () => {
                     <Th>Status</Th>
                     <Td
                       style={{
-                        borderRadius: "`10px",
+                        borderRadius: "20px",
                         background: product.status === "Shipped" ? "green" : "red",
                         color: "white",
-                        fontSize: "sm",
                       }}
                     >
                       {product.status}

39bef77fa38d8ac9f0cd9eb7702c6436c6f89392
SIDDHARTH SUTARIYA <siddharthpsutariya@gmail.com>
2024-01-01 03:17:32 +0530

    Archita_S1

--- src/components/Admin/Order/Order.jsx
+++ src/components/Admin/Order/Order.jsx
@@ -6,83 +6,74 @@ import {
   Th,
   Td,
   TableContainer,
+  Box,
 } from "@chakra-ui/react";
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-
-
 const Order = () => {
-  const [total, setTotal] = useState(0);
+  const [data, setData] = useState([]);
 
-  const [data, setData] = React.useState([]);
-
-
-  React.useEffect(() => {
-    getCartData();
+  useEffect(() => {
+    getOrdersData();
   }, []);
 
-  const getCartData = async (params) => {
-  
-    let res = await axios("/cart/getcart");
-    
-    const d = await res.data;
-    
-     
-  
-   
+  const getOrdersData = async () => {
+    try {
+      const res = await axios("/order/getallorders");
+      const ordersData = res.data.orders;
+
+      setData(ordersData);
+    } catch (error) {
+      console.error("Error fetching orders:", error);
+    }
   };
 
   return (
-    <div>
-      <h1
-        style={{
-          textAlign: "center",
-          marginTop: "10px",
-          fontSize: "20px",
-          fontWeight: "bold",
-        }}
-      >
+    <Box p={8} textAlign="center">
+      <h1 fontSize="2xl" fontWeight="bold" mb={6}>
         Recent Orders
       </h1>
-      <TableContainer>
-        <Table variant="simple">
+      <TableContainer borderRadius="md" overflow="hidden">
+        <Table variant="simple" size="lg">
           <Thead>
-            <Tr>
-              <Th>Id</Th>
+            <Tr bg="gray.100">
               <Th>Order ID</Th>
               <Th>Title</Th>
+              <Th>Status</Th>
               <Th isNumeric>Total Products</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {data?.map((items) => {
-              return (
-                <Tr>
-                  <Td fontFamily="'Philosopher', sans-serif;">{items.id}</Td>
-                  <Td>{items.orderId}</Td>
+            {data?.map((order) => (
+              <Tr key={order._id} _odd={{ bg: "gray.50" }}>
+                <Td>{order._id}</Td>
+                <Td>{order.user}</Td>
 
-                  {items.products.map((e) => (
-                    <Tr>
-                      <Th>Products</Th>
-                      <Td>{e.title}</Td>
-                      <Th>Price</Th>
-                      <Td>₹{e.price}</Td>
-                      <Th>Status</Th>
-                      <Td style={{ borderRadius: "20px", background: "green" }}>
-                        {e.status}
-                      </Td>
-                    </Tr>
-                  ))}
-                  <Td>{items.products.length}</Td>
-                </Tr>
-              );
-            })}
+                {order.items.map((product) => (
+                  <Tr key={product.productID}>
+                    <Th>Product</Th>
+                    <Td>{product.title}</Td>
+                    <Th>Status</Th>
+                    <Td
+                      style={{
+                        borderRadius: "`10px",
+                        background: product.status === "Shipped" ? "green" : "red",
+                        color: "white",
+                        fontSize: "sm",
+                      }}
+                    >
+                      {product.status}
+                    </Td>
+                  </Tr>
+                ))}
+                <Td>{order.items.length}</Td>
+              </Tr>
+            ))}
           </Tbody>
         </Table>
       </TableContainer>
-    </div>
+    </Box>
   );
 };
 
